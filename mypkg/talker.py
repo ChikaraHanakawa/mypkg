3e4debd0659cb59c2cbfcd5583f0392822ae2650
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2022-12-31 02:54:49 +0900

    test github actions

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,19 +1,24 @@
 import rclpy
 from rclpy.node import Node
-from person_msgs.msg import Person
+from std_msgs.msg import Int16
 
-rclpy.init()
-node = Node("talker")
-pub = node.create_publisher(Person, "countup" , 10)
-n = 0
+class Talker():
+  def __init__(self):
+    self.pub = node.create_publisher(Int16, "countup", 10)
+    self.n = 0
+    node.create_timer(0.5, self.cb)
 
-def cb():
-    global n
-    msg = Person()
-    msg.name = "上田隆一"
-    msg.age = n
-    pub.publish(msg)
-    n += 1
+  def cb(self):
+    msg = Int16()
+    msg.data = self.n
+    self.pub.publish(msg)
+    self.n += 1
 
-node.create_timer(0.5, cb)
-rclpy.spin(node)
+def main():
+  rclpy.init()
+  node = Node("Talker")
+  talker = Talker(node)
+  rclpy.spin(node)
+
+if __name__ == '__main__'
+  main()
