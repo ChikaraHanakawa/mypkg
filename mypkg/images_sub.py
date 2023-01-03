7b52d6762c272cdb11d3fdc212e2ea7afcfccf53
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-01-03 19:21:09 +0900

    update facedetector

--- mypkg/images_sub.py
+++ mypkg/images_sub.py
@@ -3,23 +3,51 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
+import os
+
+dirname = 'face_images'
+cascade_filepath = "/home/chikara/ros2_ws/src/mypkg/mypkg/data/haarcascades/haarcascade_frontalface_alt2.xml"
+FaceCascade = cv2.CascadeClassifier(cascade_filepath)
+
+def mosaic(img, alpha):
+  w = img.shape[1]
+  h = img.shape[0]
+  img = cv2.resize(img, (int(w*alpha), int(h*alpha)))
+  img = cv2.resize(img, (w, h), interpolation=cv2.INTER_NEAREST)
+  return img 
 
 class ImageSubscriber(Node):
   def __init__(self):
     super().__init__('image_subscriber')
     self.subscription = self.create_subscription(
       Image, 
-      'video_frames', 
-      self.listener_callback, 
+      'video_frames',
+      self.listener_callback,
       10)
-    self.subscription # prevent unused variable warning
+    self.subscription
     self.br = CvBridge()
 
   def listener_callback(self, data):
     self.get_logger().info('Receiving video frame')
     current_frame = self.br.imgmsg_to_cv2(data)
+    gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+    Face = FaceCascade.detectMultiScale(gray,1.1,3,minSize=(100,100))
+    if not os.path.exists(dirname):
+      os.mkdir(dirname)
+    if len(Face) > 0:
+      count = 0
+      for (x, y, w, h) in Face:
+        #current_frame[y:y+h, x:x+w] = mosaic(current_frame[y:y+h, x:x+w], 0.07)
+        cv2.rectangle(current_frame, (x,y),(x+w,y+h),(255,0,0),thickness=2)
+        count += 1
+        count_padded = '%05d' % count
+        write_file = count_padded + ".png"
+        cv2.imwrite(os.path.join(dirname, write_file), current_frame)
+
+    #self.get_logger().info('Receiving video frame')
+    #current_frame = self.br.imgmsg_to_cv2(data)
     cv2.imshow("camera", current_frame)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
 
 def main(args=None):
   rclpy.init(args=args)
