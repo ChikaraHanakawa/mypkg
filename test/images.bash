6f8b984d3efea0e186e297806bdb9d691a60ac91
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-01-06 14:27:39 +0900

    test githubactions

--- test/images.bash
+++ test/images.bash
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 dir=~
-[ "$1" A
-!= "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
 DIRECTORY=/face_images  #画像ファイルの絶対パス
 
-cd $dir/ros2_ws
-colcon build
-source $dir/.bashrc
+#cd $dir/ros2_ws
+#colcon build
+#source $dir/.bashrc
 if [-d $DIRECTORY ]; then  #画像ファイルの存在確認
   echo "exit frame"
 fi
