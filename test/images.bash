10b7b2dc9a681ee1caa074e746e8842ae6bf8dfa
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-01-06 09:24:13 +0900

    fix test

--- test/images.bash
+++ test/images.bash
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+[ "$1" A
+!= "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
+DIRECTORY=/home/ros2_ws/src/mypkg/mypkg/face_images  #画像ファイルの絶対パス
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-if [-f 00000.png ]; then
+if [-d $DIRECTORY ]; then  #画像ファイルの存在確認
   echo "exit frame"
 fi
