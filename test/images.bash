01109973161bea8d7308ea01a65adc57369a29d4
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-01-06 14:40:18 +0900

    test githubactions

--- test/images.bash
+++ test/images.bash
@@ -2,15 +2,11 @@
 
 dir=~
 [ "$1" != "" ] && dir="$1"   #引数があったら、そちらをホームに変える。
-DIRECTORY=/face_images  #画像ファイルの絶対パス
+DIRECTORY=/src/mypkg/mypkg/face_images  #画像ファイルの絶対パス
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-#if [ -d $DIRECTORY ]; then  画像ファイルの存在確認
-  #echo "exit frame"
-#fi
-if [ -n "$(ls -A $DIRECTORY)" ]; then
-    echo "出力先にファイルが存在します。削除してください。=> $DSTDIR"
-    exit 1
+if [ -d $DIRECTORY ]; then  #画像ファイルの存在確認
+  echo "exit frame"
 fi
